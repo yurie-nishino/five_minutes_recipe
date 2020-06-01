b0bab2f5a63f723f87fc13e43494d56cff79ceaa
yurie-nishino <nishinoyurie0314@gmail.com>
2020-06-01 21:05:35 +0900

    fme_edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   
-  before_action
+  before_acti
 
   def index
     @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(5)
